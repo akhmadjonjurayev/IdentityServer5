9d15ed52b9f0b57bf20964bdbb1162360c5a286d
Akhmadjon <shunchakiadres@gmail.com>
2022-12-09 12:35:16 +0500

    configuration

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using IdentityServer5.Extentions;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +9,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<IdentityDb>(option => {
+   option.UseNpgsql(builder.Configuration.GetConnectionString("MyConnection"));
+});
 
 builder.ConfigureIdentityServer(builder.Configuration);
 

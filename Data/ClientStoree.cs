ada42198af5cb3afd2a344c620798f515bb72039
Ahmadjon <a.jorayev@baik.uz>
2022-12-27 22:54:40 +0500

    [+] add class ResourceStore.cs

--- Data/ClientStoree.cs
+++ Data/ClientStoree.cs
@@ -1,24 +1,46 @@
 using Duende.IdentityServer.EntityFramework.DbContexts;
 using Duende.IdentityServer.Models;
 using Duende.IdentityServer.Stores;
+using Microsoft.EntityFrameworkCore;
 
 namespace IdentityServer5.Data
 {
     public class ClientStore : IClientStore
     {
-        private IdentityDb _identityDb;
         private readonly ConfigurationDbContext _configurationDbContext;
-        public ClientStore(IdentityDb identityDb, ConfigurationDbContext configurationDbContext)
+        public ClientStore(ConfigurationDbContext configurationDbContext)
         {
-            this._identityDb = identityDb;
             this._configurationDbContext = configurationDbContext;
         }
         public Task<Client> FindClientByIdAsync(string clientId)
         {
-            var client = _configurationDbContext.Clients.FirstOrDefault(l => l.ClientId == clientId);
+            var client = _configurationDbContext.Clients
+                .Include(l => l.AllowedCorsOrigins)
+                .Include(l => l.AllowedGrantTypes)
+                .Include(l => l.AllowedScopes)
+                .Include(l => l.RedirectUris)
+                .Include(l => l.PostLogoutRedirectUris)
+                .FirstOrDefault(l => l.ClientId == clientId);
             if (client is null)
                 throw new Exception("error-not-found-data");
-            return Task.FromResult(client as Client);
+
+            Client client_map = new Client
+            {
+                ClientId = client.ClientId,
+                ClientUri = client.ClientUri,
+                ClientName = client.ClientName,
+                RedirectUris = client.RedirectUris.Select(l => l.RedirectUri).ToList(),
+                PostLogoutRedirectUris = client.PostLogoutRedirectUris.Select(l => l.PostLogoutRedirectUri).ToList(),
+                AllowedCorsOrigins = client.AllowedCorsOrigins.Select(l => l.Origin).ToList(),
+                AllowAccessTokensViaBrowser = client.AllowAccessTokensViaBrowser,
+                AllowedScopes = client.AllowedScopes.Select(l => l.Scope).ToList(),
+                AllowedGrantTypes = client.AllowedGrantTypes.Select(l => l.GrantType).ToList(),
+                RequireClientSecret = client.RequireClientSecret,
+                RequireConsent = client.RequireConsent,
+                AccessTokenLifetime = client.AccessTokenLifetime
+            };
+
+            return Task.FromResult(client_map);
         }
     }
 }
---
+++ Data/ResourceStore.cs
@@ -0,0 +1,33 @@
+﻿using Duende.IdentityServer.Models;
+using Duende.IdentityServer.Stores;
+
+namespace IdentityServer5.Data
+{
+    public class ResourceStore : IResourceStore
+    {
+        public Task<IEnumerable<ApiResource>> FindApiResourcesByNameAsync(IEnumerable<string> apiResourceNames)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<ApiResource>> FindApiResourcesByScopeNameAsync(IEnumerable<string> scopeNames)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<ApiScope>> FindApiScopesByNameAsync(IEnumerable<string> scopeNames)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<IdentityResource>> FindIdentityResourcesByScopeNameAsync(IEnumerable<string> scopeNames)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Resources> GetAllResourcesAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

3f1eab749da22a537d0c87e1150718e5ac9a34df
Akhmadjon Jurayev <shunchakiadres@gmail.com>
2023-03-07 12:24:55 +0500

    [+] add UserController.cs

---
+++ IdentityServer/Service/UserService.cs
@@ -0,0 +1,18 @@
+﻿using IdentityServer5.Models;
+using IdentityServer5.ViewModels;
+
+namespace IdentityServer5.Service
+{
+    public class UserService
+    {
+        public UserService()
+        {
+
+        }
+
+        public async Task<JsonResponse> CreateUserAsync(UserViewModel viewModel)
+        {
+
+        }
+    }
+}
